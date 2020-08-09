f3c0ba3bbc45ec56e4ac9668ed7f061ea1a27d53
unknown <pradeepkm19@gmail.com>
2020-08-09 21:46:23 +0530

    UI changes

--- starter.py
+++ starter.py
@@ -1,48 +1,58 @@
-from tkinter import ttk, Tk
+import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
-import pandas as pd
-import datetime
+from ttkthemes import ThemedTk
+
 import argparse
 from interface import Interface
 
-class Root(Tk):
+class Root(ThemedTk):
     def __init__(self, args):
-        super(Root, self).__init__()
+        super(Root, self).__init__(theme="arc")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
         self.config_file = args.config_file
         self.title("SCD files import")
         self.minsize(300, 150)
 
         self.labelFrame = ttk.LabelFrame(self, text = "Import files")
-        self.labelFrame.grid(column = 4, row = 4, padx=20, pady = 20)
+        self.labelFrame.grid(column = 4, row = 4, padx=20, pady = 20,sticky=tk.N+tk.S+tk.E+tk.W)
 
         self.labelFrame_files = ttk.LabelFrame(self, text = "Selected files")
-        self.labelFrame_files.grid(column = 5, row = 4, padx=100, pady = 20)
+        self.labelFrame_files.grid(column = 5, row = 4, padx=100, pady = 20, sticky=tk.N+tk.S+tk.E+tk.W)
 
         self.buttons()
  
     def buttons(self):
         self.button = ttk.Button(self.labelFrame, text = "Select file/s", command = self.fileDialog)
-        self.button.grid(column = 2, row = 1,padx=10, pady=20)
+        self.button.grid(column = 2, row = 1,padx=10, pady=20, sticky=tk.N+tk.S+tk.E+tk.W)
         self.button = ttk.Button(self.labelFrame, text = "Import", command = self.run_scd)
-        self.button.grid(column = 1, row = 3,padx=10, pady=20)
+        self.button.grid(column = 1, row = 3,padx=10, pady=20, sticky=tk.N+tk.S+tk.E+tk.W)
         self.button = ttk.Button(self.labelFrame, text = "Cancel", command = self.quit)
-        self.button.grid(column = 3, row = 3,padx=10, pady=20)
+        self.button.grid(column = 3, row = 3,padx=10, pady=20, sticky=tk.N+tk.S+tk.E+tk.W)
 
     def fileDialog(self):
  
         self.filename = filedialog.askopenfilenames(
-            initialdir =  "/", title = "Select file/s", filetype = (("csv files","*.csv"),("all files","*.*"))
+            initialdir =  "/",
+            title = "Select file(s)",
+            filetype = (
+                ("csv files","*.csv"),
+                ("tsv files","*.tsv"),
+                ("dat files","*.dat"),
+                ("all files","*.*")
+            )
         )
 
         for i in range(len(self.filename)):
+
             self.label = ttk.Label(self.labelFrame_files, text = "Hello")
-            self.label.grid(column = 2, row = i+1)
+            self.label.grid(column = 2, row = i+1, sticky=tk.N+tk.S+tk.E+tk.W)
             self.label.configure(text = self.filename[i])
-    
 
     def run_scd(self):
-        self.destroy()
+        self.withdraw()
         Interface.execute_config(self.filename, self.config_file)
 
 
